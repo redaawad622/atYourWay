b322d6b774ecb56b3ad1b4336cae40f53654ae4a
reda awad <redaawad622@gmail.com>
2021-10-24 23:48:35 +0200

    add dynmic store id

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,5 +1,5 @@
 export default function ({ $axios }) {
   $axios.defaults.params = $axios.defaults.params || {}
-  $axios.defaults.params.store_id = '1'
-
+  $axios.defaults.params.store_id = process.env.store_id
+  console.log(process.env)
 }
