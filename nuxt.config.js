615b53e6eff1c195396f8629b1064e947d892dc0
reda awad <redaawad622@gmail.com>
2021-10-26 23:38:54 +0200

    change strore id

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,6 +14,11 @@ export default {
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
+  env: {
+    baseUrl: process.env.BASE_URL,
+    store_id: process.env.store_id,
+  },
+
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: ['~/assets/main.css'],
 
--- plugins/axios.js
+++ plugins/axios.js
@@ -1,5 +1,6 @@
 export default function ({ $axios }) {
   $axios.defaults.params = $axios.defaults.params || {}
-  $axios.defaults.params.store_id = process.env.store_id
-  console.log(process.env)
+  $axios.defaults.params.store_id = process.env.store_id || 3
+  console.log('base', process.env.baseUrl)
+  console.log('store', process.env.store_id)
 }
